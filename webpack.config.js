16c90e119af88b7d0f70165e41ec6e116672019d
Nguyen Duc Hoa <nguyenduchoa0130@gmail.com>
2023-12-01 19:31:38 +0700

    Update env

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    ],
+  },
+};
